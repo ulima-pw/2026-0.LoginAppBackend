91704d8852da0c4d3985e7ca39698af6f3b2c555
ProfL3301 <ProfL3301@lab.aca>
2026-02-16 15:38:27 -0500

    Relacion de 1 a 1

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 import uuid
-from sqlalchemy import UUID, Column, String, DateTime
+from sqlalchemy import UUID, Column, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
 class Usuario(Base):
@@ -12,6 +13,23 @@ class Usuario(Base):
     )
     username = Column(String, unique=True)
     password = Column(String, unique=True)
+    perfil = relationship("Perfil", back_populates="usuario", uselist=False)
+
+class Perfil(Base):
+    __tablename__ = "perfil"
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=lambda: str(uuid.uuid4()),
+        index=True
+    )
+    nombre = Column(String)
+    pais = Column(String)
+    usuario_id = Column(
+        String(36),
+        ForeignKey("usuario.id", unique=True)
+    )
+    usuario = relationship("Usuario", back_populates="perfil")
 
 class Acceso(Base):
     __tablename__ = "acceso"
