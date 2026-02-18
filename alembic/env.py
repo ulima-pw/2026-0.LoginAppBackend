da5614e74967597378fe27617355b569b0c8da4e
ProfL3301 <ProfL3301@lab.aca>
2026-02-18 16:42:15 -0500

    Preparando para subir a la nube render

--- alembic/env.py
+++ alembic/env.py
@@ -4,10 +4,15 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+db_url = os.getenv("DATABASE_URL")
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-CADENA_CONEXION = "postgresql://videojuegos:videojuegos@localhost:5432/bd_videojuegos"
+CADENA_CONEXION = os.getenv("DATABASE_URL")
 
 engine = create_engine(CADENA_CONEXION)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
